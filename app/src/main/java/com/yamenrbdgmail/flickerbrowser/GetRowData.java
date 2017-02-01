f1eb27764f9829f3bd0f4336305f4c0daf9ac59c
yamen_rbd <yamenrbd@gmail.com>
2017-02-01 18:37:24 +0300

    first push

--- app/src/main/java/com/yamenrbdgmail/flickerbrowser/GetRowData.java
+++ app/src/main/java/com/yamenrbdgmail/flickerbrowser/GetRowData.java
@@ -1,5 +1,15 @@
 package com.yamenrbdgmail.flickerbrowser;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by German Center on 01/02/2017.
  */
@@ -13,5 +23,91 @@ public class GetRowData {
 
     public GetRowData(String mRowUrl) {
         this.mRowUrl = mRowUrl;
+        this.mDownloadStatus = DownloadStatus.IDLE;
+
+    }
+    public void reset (String mRowUrl){
+        this.mRowUrl=null;
+        this.mData=null;
+        this.mDownloadStatus=DownloadStatus.IDLE;
+    }
+
+    public DownloadStatus getmDownloadStatus() {
+        return mDownloadStatus;
+    }
+
+    public String getmData() {
+        return mData;
+    }
+    public void execute(){
+        this.mDownloadStatus = mDownloadStatus.PROCESSING;
+        DownloadRawData downloadRawData = new DownloadRawData();
+        downloadRawData.execute(mRowUrl);
+
+    }
+
+    public class DownloadRawData extends AsyncTask<String , Void , String>{
+        protected void onPostExecute(String webData){
+            //TODO fill later
+            mData =webData;
+            Log.v(LOG_TAG,"data returned was "+mData);
+            if (mData==null){
+                if(mDownloadStatus==null){
+                    mDownloadStatus=DownloadStatus.NOT_INITIALISED;
+                }
+                else{
+                    mDownloadStatus=DownloadStatus.FAILED_OR_EMPTY;
+
+                }
+            }else{
+                //sucsses
+                mDownloadStatus=DownloadStatus.OK;
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            if(params==null){
+                return null;
+            }
+            try{
+                URL url =  new URL(params[0]);
+                urlConnection=(HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inputStream = urlConnection.getInputStream();
+
+                if (inputStream == null){
+                    return null;
+                }
+
+                StringBuffer buffer = new StringBuffer();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while((line= reader.readLine())!=null){
+                    buffer.append(line+ "\n");
+                }
+                return buffer.toString();
+
+            }catch(IOException e){
+                Log.e(LOG_TAG,"error",e);
+                return null;
+
+            }finally{
+                if (urlConnection!=null){
+                    urlConnection.disconnect();
+                }
+                if (reader!=null){
+                    try{
+                    reader.close();
+                    }catch (final IOException e){
+                        Log.e(LOG_TAG,"error closing string",e);
+                    }
+                }
+            }
+
+        }
     }
 }
--- app/src/main/java/com/yamenrbdgmail/flickerbrowser/MainActivity.java
+++ app/src/main/java/com/yamenrbdgmail/flickerbrowser/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+            GetRowData theRowData = new GetRowData("https://api.flickr.com/services/feeds/photos_public.gne?tags=android,kitkat&format=json&nojsoncallback=1");
+            theRowData.execute();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
