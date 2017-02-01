1779fb5c1c44427eebeb2617cbf78fed89ff8874
yamen_rbd <yamenrbd@gmail.com>
2017-02-01 11:36:42 +0300

    first push

--- app/src/main/java/com/yamenrbdgmail/flickerbrowser/GetRowData.java
+++ app/src/main/java/com/yamenrbdgmail/flickerbrowser/GetRowData.java
@@ -3,6 +3,15 @@ package com.yamenrbdgmail.flickerbrowser;
 /**
  * Created by German Center on 01/02/2017.
  */
+enum DownloadStatus{IDLE,PROCESSING,NOT_INITIALISED,FAILED_OR_EMPTY,OK};
 
 public class GetRowData {
+    private String LOG_TAG = GetRowData.class.getSimpleName();
+    private String mRowUrl;
+    private String mData;
+    private DownloadStatus mDownloadStatus;
+
+    public GetRowData(String mRowUrl) {
+        this.mRowUrl = mRowUrl;
+    }
 }
