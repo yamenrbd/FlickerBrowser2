01f9d65cb738a6b6ff6af7a14eb3a7224100c211
yamen_rbd <yamenrbd@gmail.com>
2017-02-02 10:30:09 +0300

    third push

---
+++ app/src/main/java/com/yamenrbdgmail/flickerbrowser/GetFlickerJsonData.java
@@ -0,0 +1,12 @@
+package com.yamenrbdgmail.flickerbrowser;
+
+/**
+ * Created by German Center on 02/02/2017.
+ */
+
+public class GetFlickerJsonData extends GetRowData {
+    public GetFlickerJsonData (String searchCriteria , boolean matchAll){
+        super(null);
+
+    }
+}
--- app/src/main/java/com/yamenrbdgmail/flickerbrowser/Photo.java
+++ app/src/main/java/com/yamenrbdgmail/flickerbrowser/Photo.java
@@ -5,5 +5,55 @@ package com.yamenrbdgmail.flickerbrowser;
  */
 
 public class Photo {
-    private String title;
+    private String mtitle;
+    private String mAuthor;
+    private String mAuthorID;
+    private String mLink;
+    private String mTags;
+    private String mImage;
+
+    public Photo(String mtitle, String mAuthor, String mAuthorID, String mLink, String mTags, String mImage) {
+        this.mtitle = mtitle;
+        this.mAuthor = mAuthor;
+        this.mAuthorID = mAuthorID;
+        this.mLink = mLink;
+        this.mTags = mTags;
+        this.mImage = mImage;
+    }
+
+    public String getMtitle() {
+        return mtitle;
+    }
+
+    public String getmAuthor() {
+        return mAuthor;
+    }
+
+    public String getmAuthorID() {
+        return mAuthorID;
+    }
+
+    public String getmLink() {
+        return mLink;
+    }
+
+    public String getmTags() {
+        return mTags;
+    }
+
+    public String getmImage() {
+        return mImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" +
+                "mtitle='" + mtitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mAuthorID='" + mAuthorID + '\'' +
+                ", mLink='" + mLink + '\'' +
+                ", mTags='" + mTags + '\'' +
+                ", mImage='" + mImage + '\'' +
+                '}';
+    }
 }
